4cbf566e5ae808080d7f98f72b17fa8fce70823a
Michael <mihail.tchernov@yandex.ru>
2021-04-29 07:07:04 +0000

    First lesson was finished

--- main.py
+++ main.py
@@ -4,12 +4,27 @@ import curses
 import asyncio
 
 from fire_animation import fire
+from sky_animation import stars_light
+
+from curses_tools import draw_frame
+from curses_tools import read_controls
 
 
 SYMBOLS = ('+', '*', '.', ':')
 AMIMATION_DELAY = 0.1
 MIN_STARS_COUNT = 50
 MAX_STARS_COUNT = 200
+
+SPACE_FRAMES = []
+SPACE_X_SIZE, SPACE_Y_SIZE = 5, 9
+SPACE_X_SPEED, SPACE_Y_SPEED = 2, 2
+with open('spaceFrames/frame_0.txt', 'r') as f:
+  text = ''.join(f.readlines())
+  SPACE_FRAMES.append(text)
+
+with open('spaceFrames/frame_1.txt', 'r') as f:
+  text = ''.join(f.readlines())
+  SPACE_FRAMES.append(text)
 
 
 async def blink(canvas, row, column, sleeping_time, symbol='*'):
@@ -33,65 +48,45 @@ async def blink(canvas, row, column, sleeping_time, symbol='*'):
       await asyncio.sleep(0)
 
 
-def add_star(canvas):
-  row_index, column_index = 5, 20
+async def space_animation(canvas):
+  canvas.nodelay(True)
+  x_max, y_max = canvas.getmaxyx()
+  row_pos, col_pos = x_max // 2, y_max // 2
+  i = 0
   while True:
-    canvas.addstr(row_index, column_index, '*', curses.A_DIM)
-    canvas.border()
-    canvas.refresh()
-    time.sleep(2)
-    canvas.addstr(row_index, column_index, '*')
-    canvas.refresh()
-    time.sleep(0.3)
-    canvas.addstr(row_index, column_index, '*', curses.A_BOLD)
-    canvas.refresh()
-    time.sleep(0.5)
-    canvas.addstr(row_index, column_index, '*')
-    canvas.refresh()
-    time.sleep(0.3)
+    rows_direction, columns_direction, _ = read_controls(canvas)
+    row_pos += rows_direction * SPACE_X_SPEED
+    col_pos += columns_direction * SPACE_Y_SPEED
+    if row_pos < 1:
+      row_pos = 1
+    if row_pos > x_max - 2 - SPACE_Y_SIZE:
+      row_pos = x_max - 2 - SPACE_Y_SIZE
 
+    if col_pos < 1:
+      col_pos = 1
+    if col_pos > y_max - 2 - SPACE_X_SIZE:
+      col_pos =  y_max - 2 - SPACE_X_SIZE
 
-def draw(canvas):
-  canvas.border()
-  coroutine = blink(canvas, 5, 20)
-  while True:
-    try:
-      coroutine.send(None)
-    except StopIteration:
-      break
-    canvas.refresh()
-    time.sleep(0.1)
-
-
-def draw_five_stars(canvas):
-  canvas.border()
-  coroutines = []
-  for i in range(5):
-    coroutines.append(blink(canvas, 5, 20 + 2 * i))
-
-  while True:
-    for coroutine in coroutines:
-      try:
-        coroutine.send(None)
-      except StopIteration:
-        break
-    canvas.refresh()
-    time.sleep(AMIMATION_DELAY)
+    draw_frame(canvas, row_pos, col_pos, SPACE_FRAMES[i])
+    await asyncio.sleep(0)
+    draw_frame(canvas, row_pos, col_pos, SPACE_FRAMES[i], negative=True)
+    i = (i + 1) % 2
 
 
 def random_stars(canvas):
   stars_amount = random.randint(MIN_STARS_COUNT, MAX_STARS_COUNT)
-  row_max, col_max = canvas.getmaxyx()
+  x_max, y_max = canvas.getmaxyx()
 
-  col_center = col_max // 2
+  col_center = y_max // 2
   coords = []
   for i in range(stars_amount):
-    row = random.randint(1, row_max-2)
-    col = random.randint(1, col_max-2)
+    row = random.randint(1, x_max-2)
+    col = random.randint(1, y_max-2)
     coords.append((row, col))
   
   coroutines = [blink(canvas, coords[x][0], coords[x][1], random.randint(0, 20), random.choice(SYMBOLS)) for x in range(stars_amount)]
-  shot_coroutine = fire(canvas, row_max -1, col_center)
+  # shot_coroutine = fire(canvas, x_max -1, col_center)
+  space_coroutine = space_animation(canvas)
   canvas.border()
   curses.curs_set(False)
   while True:
@@ -103,16 +98,33 @@ def random_stars(canvas):
     if len(coroutines) == 0:
       break
 
-    if shot_coroutine is not None:
-      try:
-        shot_coroutine.send(None)
-      except StopIteration:
-        shot_coroutine = None
+    # if shot_coroutine is not None:
+    #   try:
+    #     shot_coroutine.send(None)
+    #   except StopIteration:
+    #     shot_coroutine = None
+    
+    space_coroutine.send(None)
  
     canvas.refresh()
     time.sleep(AMIMATION_DELAY)
 
 
+def sky(canvas):
+  x_max, y_max = canvas.getmaxyx()
+  col_center = y_max // 2
+  coroutines = [stars_light(canvas), fire(canvas, x_max - 1, col_center)]
+  while True:
+    for coroutine in coroutines:
+      try:
+        coroutine.send(None)
+      except StopIteration:
+        coroutines.remove(coroutine)
+    if len(coroutines) == 0:
+      break
+
+
+
 if __name__ == '__main__':
   curses.update_lines_cols()
-  curses.wrapper(random_stars)
+  curses.wrapper(sky)
